aff1a1d8fe1a68fb6ece3a45b7d8d399161fc985
Amit Samra <Amit.Samra@outlook.com>
2020-11-16 01:18:24 -0500

    Removed date subset

--- nyc_crime_api.py
+++ nyc_crime_api.py
@@ -50,14 +50,25 @@ start_year=2006
 end_year=2006
 
 for i in range(start_year, end_year+1, 1):
+    
     results = client.get("8h9b-rp9u", where="arrest_date between "+"'"+str(i)+"-01-01'"+" and "+"'"+str(i)+"-01-31'", limit=10000000)
     results_df = pd.DataFrame.from_records(results)
+    
     results_df = results_df.drop('lon_lat',1)
+    
     results_df['arrest_key'] = results_df['arrest_key'].str.strip()
     results_df['arrest_key'] = results_df['arrest_key'].astype(int)
+    
     results_df['arrest_date'] = results_df['arrest_date'].astype(str)
     results_df['arrest_date'] = results_df['arrest_date'].str.slice(0,10)
-    results_06_df['arrest_date'] = pd.to_datetime(results_06_df['arrest_date'])
+    results_df['arrest_date'] = pd.to_datetime(results_df['arrest_date'])
+    
+    results_df.loc[~results_df.pd_cd.dtype == int, 'pd_cd'] = 0
+    results_df['pd_cd'] = results_df['pd_cd'].astype(float)
+    results_df['pd_cd'] = results_df['pd_cd'].astype(int)
+    
+    
+    
     results_df['arrest_precinct'] = results_df['arrest_precinct'].astype(int)
     results_df['x_coord_cd'] = results_df['x_coord_cd'].astype(float)
     results_df['y_coord_cd'] = results_df['y_coord_cd'].astype(float)
