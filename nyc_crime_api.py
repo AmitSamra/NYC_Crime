5b790be65da3196467917a8de0fc22e4d2142810
Amit Samra <Amit.Samra@outlook.com>
2020-11-16 00:28:13 -0500

    Formatted columns

--- nyc_crime_api.py
+++ nyc_crime_api.py
@@ -52,8 +52,16 @@ end_year=2006
 for i in range(start_year, end_year+1, 1):
     results = client.get("8h9b-rp9u", where="arrest_date between "+"'"+str(i)+"-01-01'"+" and "+"'"+str(i)+"-01-31'", limit=10000000)
     results_df = pd.DataFrame.from_records(results)
+    results_df = results_df.drop('lon_lat',1)
+    results_df['arrest_key'] = results_df['arrest_key'].str.strip()
+    results_df['arrest_key'] = results_df['arrest_key'].astype(int)
     results_df['arrest_date'] = pd.to_datetime(results_df['arrest_date'])
-    del results_df['lon_lat']
-    results_df.to_sql('arrests', con=engine, if_exists='append', index=False)
+    results_df['arrest_precinct'] = results_df['arrest_precinct'].astype(int)
+    results_df['x_coord_cd'] = results_df['x_coord_cd'].astype(float)
+    results_df['y_coord_cd'] = results_df['y_coord_cd'].astype(float)
+    results_df['latitude'] = results_df['latitude'].astype(float)
+    results_df['longitude'] = results_df['longitude'].astype(float)
+    results_df.to_sql('arrests', con=engine, index=False)
     results_df.to_csv(f"./raw_data/{i}.csv", index=False)
 
+    
