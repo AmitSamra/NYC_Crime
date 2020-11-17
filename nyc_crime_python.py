2f570fee491c171bd954a580fe579e2b1913c061
Amit Samra <Amit.Samra@outlook.com>
2020-11-17 05:49:54 -0500

    Updated README for logos

--- nyc_crime_python.py
+++ nyc_crime_python.py
@@ -1,9 +1,7 @@
 from sodapy import Socrata
 import pandas as pd
-from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-import csv
 from sqlalchemy import create_engine
 
 dotenv_local_path = '.env'
@@ -22,26 +20,6 @@ password=os.environ.get("NYC_password")
 
 # ------------------------------------------------------------------------------------------
 
-# Connect to MongoDB
-
-mongo_client=MongoClient()
-mongo_database = mongo_client['nyc_crime']
-mongo_collection = mongo_database['arrests']
-
-# ------------------------------------------------------------------------------------------
-
-# Save results from API in CSV by year
-
-start_year=2006
-end_year=2019
-
-for i in range(start_year, end_year+1, 1):
-    results = client.get("8h9b-rp9u", where="arrest_date between "+"'"+str(i)+"-01-01'"+" and "+"'"+str(i)+"-12-31'", limit=10000000)
-    results_df = pd.DataFrame.from_records(results)
-    results_df.to_csv(f"./raw_data/{i}.csv", index=False)
-
-# ------------------------------------------------------------------------------------------
-
 # Connect to MySQL
 
 engine = create_engine('mysql+pymysql://' + os.environ.get("MYSQL_USER") + ":" + os.environ.get("MYSQL_PASSWORD") + '@localhost:3306/nyc_crime')
@@ -49,9 +27,10 @@ engine = create_engine('mysql+pymysql://' + os.environ.get("MYSQL_USER") + ":" +
 # Create table arrests
 
 #engine.execute("DROP TABLE IF EXISTS arrests;")
+
 engine.execute(
 """
-CREATE TABLE arrests (
+CREATE TABLE IF NOT EXISTS arrests (
 arrest_key bigint not null primary key,
 arrest_date date,
 pd_cd int,
@@ -73,6 +52,8 @@ longitude numeric(15,10)
 );
 """
 )
+
+# ------------------------------------------------------------------------------------------
 
 # Retrieve data from API, tranform and ingest into MySQL
 
@@ -177,5 +158,4 @@ for i in range(start_year, end_year+1, 1):
     WHERE arrest_key NOT IN (SELECT arrest_key FROM arrests);
     """)
     
-    results_df.to_csv(f"./raw_data/{i}.csv", index=False)
-
+    #results_df.to_csv(f"./raw_data/{i}.csv", index=False)
